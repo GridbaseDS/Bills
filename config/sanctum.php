e65521c18fe9151569da9f4c41fa91178f1afe14
Samuel Diaz Pilier <sam@192.168.1.16>
2026-09-06 13:34:28 -0400

    fix(sanctum): add bdemo.gridbase.com.do to stateful domains

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,bills.gridbase.com.do,',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,bills.gridbase.com.do,bdemo.gridbase.com.do,beta.gridbase.com.do,',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
